3c0ca6a8f9c0d57d84061263a74436955b665ecc
davidbcmos <davidbcmos@gmail.com>
2025-10-22 20:25:02 +0200

    Update conexion.php

--- apps/web-php/home-clothing-tester/config/conexion.php
+++ apps/web-php/home-clothing-tester/config/conexion.php
@@ -1,7 +1,5 @@
 <?php
 $host = "localhost";            // Dirección del servidor MySQL (localhost = mismo equipo)
-$usuario = "root";              // Usuario de la base de datos
-$password = "";                 // Contraseña del usuario (vacía en este caso)
 $bd = "home_clothing_tester";   // Nombre de la base de datos a la que se quiere conectar
 
 $conn = new mysqli($host, $usuario, $password, $bd);
